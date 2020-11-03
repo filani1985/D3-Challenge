962ec691a3f6cf69688978c09452a3f6e6ef976f
filani1985 <69080772+filani1985@users.noreply.github.com>
2020-11-03 22:20:00 +0530

    update data path

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -24,7 +24,7 @@ var chartGroup = svg.append("g")
 d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
 // Import Data
-d3.csv("/assets/data/data.csv", function(err, healthData) {
+d3.csv("/D3_data_journalism/StarterCode/assets/data/data.csv", function(err, healthData) {
   if (err) throw err;
 console.log(healthData)
   // Step 1: Parse Data/Cast as numbers
